ec09b1abc631760fb678b43d917ddd99b67ee673
Uday <raghavudaypratap7@gmail.com>
2025-02-25 23:34:26 +0530

    Create 01_basic.js

---
+++ 01_basic.js
@@ -0,0 +1,9 @@
+const user1= {
+    name: "Uday Pratap Raghav",
+    age:19,
+    mobile:"98XXXXXX43",
+    IG:"@raghavuday.exe",
+    email:"raghavudaypratap7@gmail.com",
+
+};
+console.log(user1);
