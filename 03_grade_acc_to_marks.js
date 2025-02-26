130bce91ea9af6cf751c96a7074a68eccd3c73ac
Uday <raghavudaypratap7@gmail.com>
2025-02-26 14:52:58 +0530

    Create 03_grade_acc_to_marks.js

---
+++ 03_grade_acc_to_marks.js
@@ -0,0 +1,18 @@
+let marks = 89;
+let grade;
+if (marks >= 100 && marks <= 80) {
+    grade = "A"
+}
+else if (marks <= 100 && marks >= 80) {
+    grade = "A"
+}
+else if (marks <= 79 && marks >= 60) {
+    grade = "A"
+}
+else if (marks <= 59 && marks >= 33) {
+    grade = "A"
+}
+else{
+    grade = "Fail"
+}
+console.log(grade);
